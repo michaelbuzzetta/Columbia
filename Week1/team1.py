7beead26f501edec384fab70118162332ae313ad
olfrazer <olfrazer@packer.edu>
2026-07-01 15:49:39 -0400

    Team 1 Work

--- Week1/team1.py
+++ Week1/team1.py
@@ -1,4 +1,29 @@
+num = int(input("Please enter a number: "))
+
 def main():
     print("Start Here")
+    print(num)
+
+    user = str(input("Choose one of the following: \nSquaring, Cubing, Square Rooting "))
+
+    if(user == "Squaring"):
+        square()
+    
+    if(user == "Cubing"):
+        cube()
+
+
+def square():
+   sq = num * num 
+   print(sq)
+
+square()
+
+def cube():
+    cub = num * num * num
+    print(cub)
+    
+cube()
 
 main()
+
