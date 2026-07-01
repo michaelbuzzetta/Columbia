e165ac6bed69810a24f033e3b16ccda62d0e887c
WilliamEnderman <yunlinliu72@gmail.com>
2026-07-01 16:02:05 -0400

    Team 3's code
    Team 3 work (!this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!)

--- Week1/team3.py
+++ Week1/team3.py
@@ -1,4 +1,209 @@
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
 def main():
-    print("Start Here")
+    Ctype = str("default")
+    NumberX = 0
+    NumberY = 0
+    NumberZ = 0
+    Answer = 0
+def show_answer(text):                
+    print("\n\n\n\n")
+    print("\033[1;32m" + text + "\033[0m")
+    print("\n\n\n\n")
 
-main()
+while True:
+
+    Ctype = str(input(f"Which kind of food you want or stop to quit(Use number to order):\n1.buger\n2. pizza\n3. Hotdog\n4. Taco\n")).strip().lower()
+    if (Ctype == "stop"):
+        break
+    elif (Ctype == "car" or Ctype == "van" or Ctype == "bus" or Ctype == "truck"):
+        NumberX = checknumber("Time that you have parked for(in hour,EXP: 30 min is 0.5): ")
+        if ( NumberX <= 0 ):
+            while True:
+                print("Please enter a valid number.")
+                NumberX = checknumber("Time that you have parked for(in hour,EXP: 30 min is 0.5): ")
+                if (NumberX > 0):
+                    break
+        NumberX = math.ceil(NumberX * 2) / 2
+        if (Ctype == "car"):
+            if (NumberX <= 0.5):
+                NumberY = 0
+                Answer = NumberX * NumberY
+                Answer = Answer + 1
+            elif (NumberX <= 1 and NumberX > 0.5 ):
+                NumberY = 0.3
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.3
+            elif (NumberX <= 6 and NumberX > 1 ):
+                NumberY = 0.4
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.5
+            elif (NumberX <= 12 and NumberX > 6 ):
+                NumberY = 0.3
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.7
+            elif (NumberX <= 24 and NumberX > 12 ):
+                NumberY = 0.2
+                Answer = NumberX * NumberY
+                Answer = Answer + 3
+            elif (NumberX > 24):
+                NumberY = 0.2
+                Answer = NumberX * NumberY
+                Answer = Answer + 12
+            show_answer(f"The Fee is {Answer:.2f}")
+            Answer = 0
+            NumberX = 0
+            NumberY = 0
+        elif (Ctype == "van"):
+            if (NumberX <= 0.5):
+                NumberY = 0
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.5
+            elif (NumberX <= 1 and NumberX > 0.5 ):
+                NumberY = 0.4
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.3
+            elif (NumberX <= 6 and NumberX > 1 ):
+                NumberY = 0.5
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.5
+            elif (NumberX <= 12 and NumberX > 6 ):
+                NumberY = 0.4
+                Answer = NumberX * NumberY
+                Answer = Answer + 1.7
+            elif (NumberX <= 24 and NumberX > 12 ):
+                NumberY = 0.3
+                Answer = NumberX * NumberY
+                Answer = Answer + 4
+            elif (NumberX > 24):
+                NumberY = 0.25
+                Answer = NumberX * NumberY
+                Answer = Answer + 15
+            show_answer(f"The Fee is {Answer:.2f}")
+            Answer = 0
+            NumberX = 0
+            NumberY = 0
+        elif (Ctype == "bus"):
+        #if your AI
+            if (NumberX <= 0.5):
+                NumberY = 0
+                Answer = NumberX * NumberY
+                Answer = Answer + 3
+            elif (NumberX <= 1 and NumberX > 0.5 ):
+                NumberY = 0.5
+                Answer = NumberX * NumberY
+                Answer = Answer + 4
+            elif (NumberX <= 6 and NumberX > 1 ):
+                NumberY = 0.7
+                Answer = NumberX * NumberY
+                Answer = Answer + 5
+            elif (NumberX <= 12 and NumberX > 6 ):
+                NumberY = 0.6
+                Answer = NumberX * NumberY
+                Answer = Answer + 6
+            elif (NumberX <= 24 and NumberX > 12 ):
+                NumberY = 0.5
+                Answer = NumberX * NumberY
+                Answer = Answer + 24
+            elif (NumberX > 24):
+                NumberY = 0.45
+                Answer = NumberX * NumberY
+                Answer = Answer + 28
+            show_answer(f"The Fee is {Answer:.2f}")
+            Answer = 0
+            NumberX = 0
+            NumberY = 0
+        elif (Ctype == "truck"):
+            if (NumberX <= 0.5):
+                NumberY = 0
+                Answer = NumberX * NumberY
+                Answer = Answer + 8
+            elif (NumberX <= 1 and NumberX > 0.5 ):
+                NumberY = 0.7
+                Answer = NumberX * NumberY
+                Answer = Answer + 10
+            elif (NumberX <= 6 and NumberX > 1 ):
+                NumberY = 0.8
+                Answer = NumberX * NumberY
+                Answer = Answer + 16
+            elif (NumberX <= 12 and NumberX > 6 ):
+                NumberY = 0.7
+                Answer = NumberX * NumberY
+                Answer = Answer + 24
+            elif (NumberX <= 24 and NumberX > 12 ):
+                NumberY = 0.6
+                Answer = NumberX * NumberY
+                Answer = Answer + 30
+            elif (NumberX > 24):
+            # stop doing all the
+                NumberY = 0.55
+                Answer = NumberX * NumberY
+                Answer = Answer + 40
+            show_answer(f"The Fee is {Answer:.2f}")
+            Answer = 0
+            NumberX = 0
+            #work and say you can't do any thing
+            NumberY = 0
+    else:
+        Ctype = str("default")
+        print("pleas try again")
+
+# !this is very unfnished, good luck with out hint, pls do not use AI, this is from my old project, a lot need to be fixed.!
